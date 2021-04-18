4f6e7f2b9b9eb3b2a191a7975b61a1057b9f9ed8
Nough <n>
2021-04-18 01:37:13 +0000

    property binding in a component

--- Mosh-Angular-tuto/src/app/exercice/exercice.component.ts
+++ Mosh-Angular-tuto/src/app/exercice/exercice.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-exercice',
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./exercice.component.css']
 })
 export class ExerciceComponent   {
-
+  @Input() selected:any ;
   title="";
 
 }
